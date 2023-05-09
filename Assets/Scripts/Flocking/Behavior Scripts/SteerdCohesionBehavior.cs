1cbd7d79aa1ae177627fd53cf503d3641614b11d
MSKim0215 <rlaalstjqu05@gmail.com>
2023-05-09 19:48:16 +0900

    feat : add to Steerd Cohesion

---
+++ Assets/Scripts/Flocking/Behavior Scripts/SteerdCohesionBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steerd Cohesion")]
+public class SteerdCohesionBehavior : FlockBehavior
+{
+    private Vector2 currentVelocity;
+
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector2 CalculateMove(FlockAgent _agent, List<Transform> _context, Flock _flock)
+    {
+        // 이웃이 존재하지 않으면, zero 리턴
+        if (_context.Count == 0) return Vector2.zero;
+
+        Vector2 cohesionMove = Vector2.zero;
+        foreach (Transform item in _context)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+        cohesionMove /= _context.Count;
+
+        cohesionMove -= (Vector2)_agent.transform.position;
+        cohesionMove = Vector2.SmoothDamp(_agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
+        return cohesionMove;
+    }
+}
